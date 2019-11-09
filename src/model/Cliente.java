63abd24d7caecc76005e9819e3f57716558d0e0d
Renan Cavalcante <renan.cav.ti@gmail.com>
2019-11-09 18:26:04 -0300

    New atributes and new classes

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,5 +1,41 @@
 package model;
 
 public class Cliente {
-
+	
+	private long id;
+	private String nome;
+	private String cpf;
+	private String email;
+	private Endereco endereco;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
 }
--- src/model/Funcionario.java
+++ src/model/Funcionario.java
@@ -1,5 +1,56 @@
 package model;
 
 public class Funcionario {
+	
+	private String id;
+	private String nome;
+	private String email;
+	private String rg;
+	private String clt;
+	private String cnpj;
+	private Endereco endereco;
+	
 
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getClt() {
+		return clt;
+	}
+	public void setClt(String clt) {
+		this.clt = clt;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
 }
