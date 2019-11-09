4e231af8b4aed1acec75b4c22c345f850466ea0c
Renan Cavalcante <renan.cav.ti@gmail.com>
2019-11-09 16:22:06 -0300

    Updates and creation of UIs

---
+++ src/ui/EmpTerceirizadaUI.java
@@ -0,0 +1,9 @@
+package ui;
+
+public class EmpTerceirizadaUI {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/ui/VendaUI.java
@@ -0,0 +1,9 @@
+package ui;
+
+public class VendaUI {
+
+	public static void main(String[] args) {
+
+	}
+
+}
