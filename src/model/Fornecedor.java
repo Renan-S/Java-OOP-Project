e8da2bea06e996b89abd865d14690f123d58d2bf
rts30 <49524342+rts30@users.noreply.github.com>
2019-11-14 13:51:43 -0300

    Mudança na Classe Fornecedor

--- src/model/Fornecedor.java
+++ src/model/Fornecedor.java
@@ -2,4 +2,48 @@ package model;
 
 public class Fornecedor {
 
+  private String cnpj;
+	private int id;
+	private String nome;
+	private Produto produto;
+	private String email;
+	private Endereco endereco;
+	
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+  
 }
