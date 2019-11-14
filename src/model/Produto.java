266a81c610e24da64d67b13682e173466cff7105
rts30 <49524342+rts30@users.noreply.github.com>
2019-11-14 14:05:43 -0300

    Alteração na Classe Produto

--- src/model/Produto.java
+++ src/model/Produto.java
@@ -1,5 +1,52 @@
 package model;
 
 public class Produto {
+  private int id;
+	private String tipo;
+	private String nome;
+	private double valor;
+	private int quant;
+	
+	public Produto(String tipo, String nome, double valor, int quant) {
+		this.tipo = tipo;
+		this.nome = nome;
+		this.valor = valor;
+		this.quant = quant;
+	}
+	
+	public Produto() {
+		super();
+	}
+
+	public int getid() {
+		return id;
+	}
+	public void setid(int id) {
+		this.id = id;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	public int getQuant() {
+		return quant;
+	}
+	public void setQuant(int quant) {
+		this.quant = quant;
+	}
 
 }
