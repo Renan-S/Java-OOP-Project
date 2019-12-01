27794c4cb3ae224941530d7163a8dc8fb06ba0fa
rts30 <49524342+rts30@users.noreply.github.com>
2019-12-01 12:59:10 -0300

    Update Fornecedor.java

--- src/model/Fornecedor.java
+++ src/model/Fornecedor.java
@@ -1,41 +1,35 @@
 package model;
 
-public class Fornecedor {
-	private String nome;
+public class Fornecedor extends Pessoa {
+	//private String nome;
 	private String responsavel; 
 	private String cnpj;
-	private Endereco endereco;
+	//private Endereco endereco;
 	private Produto produto;
-	private String email;
+	//private String email;
 	
-	//(Cadastrará produto)
+	public Fornecedor() {
+		
+	}
 	
+	public Fornecedor(String nome, String sexo, String email, String cpf, Endereco endereco) {
+		super(nome, sexo, email, cpf, endereco);
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("Fornecedor [nome=").append(nome).append(", responsavel=").append(responsavel).append(", cnpj=")
-				.append(cnpj).append(", endereco=").append(endereco).append(", produto=").append(produto)
-				.append(", email=").append(email).append("]");
+		builder.append("Fornecedor (responsavel=").append(responsavel).append(", cnpj=").append(cnpj)
+				.append(", produto=").append(produto).append(")");
 		return builder.toString();
 	}
 	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
 	public String getResponsavel() {
 		return responsavel;
 	}
 	public void setResponsavel(String responsavel) {
 		this.responsavel = responsavel;
-	}
-	public Endereco getEndereco() {
-		return endereco;
-	}
-	public void setEndereco(Endereco endereco) {
-		this.endereco = endereco;
 	}
 	
 	public Produto getProduto() {
@@ -44,12 +38,7 @@ public class Fornecedor {
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+	
 	public String getCnpj() {
 		return cnpj;
 	}
