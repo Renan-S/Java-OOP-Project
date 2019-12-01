04d7616ce92edfb21efdbcb497ab310c781ab1da
rts30 <49524342+rts30@users.noreply.github.com>
2019-12-01 17:33:41 -0300

    Update Constante.java

--- src/util/Constante.java
+++ src/util/Constante.java
@@ -1,5 +1,11 @@
 package util;
 
 public final class Constante {
+	
+	public static final String NOME_PROGRAMA = "Farmacia Massa";
+	public static final String VERSAO = "1.0.1.2";
+	public static final double icms = 0.05;
+	public static final double iss = 0.1;
+	public static final double ipi = 0.07; 
 
 }
